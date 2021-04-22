aebc233ed594ab8091e3bf22a7daecb7dd3e8638
Zakaria Touzri <ztouzri@c2r2p4.42nice.fr>
2021-04-22 18:07:42 +0200

    ADDED: if no wp database -> create

--- srcs/init.sh
+++ srcs/init.sh
@@ -2,10 +2,10 @@
 
 service nginx start
 service mysql start
-mysql -e "CREATE DATABASE wordpress CHARACTER SET utf8"
+mysql -e "CREATE DATABASE IF NOT EXISTS wordpress CHARACTER SET utf8"
 mysql -e "USE wordpress"
 mysql -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'changeme';"
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost';"
-#mysql -e "FLUSH PRIVILEGES"
+mysql -e "FLUSH PRIVILEGES"
 service php7.3-fpm start
 bash
